cb9f6eaf698d063528145c69c99dff04756080ca
hesy87 <hesam.mahmoodi@gmail.com>
2022-01-12 00:18:18 +0330

    created first app
    created first app in 3 pages with home about contacts

---
+++ website/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index_view (request) :
+    return HttpResponse('<h1>HOME PAGE</h1>') 
+
+def about_view (request) :
+    return HttpResponse('<h1>ABOUT PAGE</h1>') 
+
+def contact_view (request) :
+    return HttpResponse('<h1>CONTACT PAGE</h1>')     
